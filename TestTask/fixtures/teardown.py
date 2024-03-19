33079007497f95df52407c8f3c31a961ae2ab28e
arkadiy16 <arkadiy160702@gmail.com>
2024-03-20 00:59:23 +0300

    Fixtures module learned

---
+++ TestTask/fixtures/teardown.py
@@ -0,0 +1,25 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+@pytest.fixture
+def browser():
+    print('\nStart new browser for test')
+    driver = webdriver.Chrome()
+    yield driver
+    print('\n Quit browser')
+    driver.quit()
+
+
+class TestMainPage1():
+    def test_guest_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, '#login_link')
+
+    def test_guest_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, '.bascket-mini .btn-group > a')
+
